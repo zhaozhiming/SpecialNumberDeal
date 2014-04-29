f85493bb2193b353e0654c28d8f5e0913d30dfe4
zhaozhiming <kingzzm1982@sina.com>
2014-04-29 21:56:35 +0800

    refactor code

--- src/main/java/com/github/zzm/SpecialNumberDeal.java
+++ src/main/java/com/github/zzm/SpecialNumberDeal.java
@@ -7,14 +7,18 @@ import java.util.List;
 public class SpecialNumberDeal {
     public static List<String> deal(int[] specialNumbers, int loopTimes) {
         List<String> result = Lists.newArrayList();
-        for (int i = 1; i <= loopTimes; i++) {
-            if (i % specialNumbers[0] != 0) {
-                result.add(i + "");
+        for (int index = 1; index <= loopTimes; index++) {
+            if (isMultiple(specialNumbers, index)) {
+                result.add(index + "");
                 continue;
             }
 
             result.add("Fizz");
         }
         return result;
+    }
+
+    private static boolean isMultiple(int[] specialNumbers, int index) {
+        return index % specialNumbers[0] != 0;
     }
 }
