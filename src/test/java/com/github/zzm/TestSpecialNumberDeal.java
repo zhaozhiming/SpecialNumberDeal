0c9656254e8fbc9f45ed00c6a21736290fed7b4d
zhaozhiming <kingzzm1982@sina.com>
2014-04-29 21:50:33 +0800

    add 2rd test case

--- src/test/java/com/github/zzm/TestSpecialNumberDeal.java
+++ src/test/java/com/github/zzm/TestSpecialNumberDeal.java
@@ -12,4 +12,16 @@ public class TestSpecialNumberDeal {
     public void should_return_correct_result_when_given_special_number_3_and_loop_3_times() throws Exception {
         assertThat(SpecialNumberDeal.deal(new int[]{3}, 3), is(asList("1", "2", "Fizz")));
     }
+
+    @Test
+    public void should_return_correct_result_when_given_special_number_3_and_loop_6_times() throws Exception {
+        assertThat(SpecialNumberDeal.deal(new int[]{3}, 6), is(asList("1", "2", "Fizz", "4", "5", "Fizz")));
+    }
+
+    @Test
+    public void should_return_correct_result_when_given_special_number_3_5_and_loop_6_times() throws Exception {
+        assertThat(SpecialNumberDeal.deal(new int[]{3, 5}, 6), is(asList("1", "2", "Fizz", "4", "Buzz", "Fizz")));
+    }
+
+
 }
