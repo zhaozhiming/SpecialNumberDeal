519b58354b9673dad592d9a7cd30e286b9fa24dd
zhaozhiming <kingzzm1982@sina.com>
2014-04-29 22:25:00 +0800

    add 3rd test case

--- src/main/java/com/github/zzm/SpecialNumberDeal.java
+++ src/main/java/com/github/zzm/SpecialNumberDeal.java
@@ -1,24 +1,40 @@
 package com.github.zzm;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
 public class SpecialNumberDeal {
+    private static final ImmutableMap<Integer, String> NUMBER_STRING = ImmutableMap.of(
+            3, "Fizz",
+            5, "Buzz"
+    );
+
     public static List<String> deal(int[] specialNumbers, int loopTimes) {
         List<String> result = Lists.newArrayList();
         for (int index = 1; index <= loopTimes; index++) {
-            if (isMultiple(specialNumbers, index)) {
+            if (!isSpecial(specialNumbers, index)) {
                 result.add(index + "");
                 continue;
             }
 
-            result.add("Fizz");
+            result.add(NUMBER_STRING.get(whichSpecialNumber(specialNumbers, index)));
         }
         return result;
     }
 
-    private static boolean isMultiple(int[] specialNumbers, int index) {
-        return index % specialNumbers[0] != 0;
+    private static int whichSpecialNumber(int[] specialNumbers, int index) {
+        for (int specialNumber : specialNumbers) {
+            if (index % specialNumber == 0) return specialNumber;
+        }
+        throw new RuntimeException("error");
+    }
+
+    private static boolean isSpecial(int[] specialNumbers, int index) {
+        for (int specialNumber : specialNumbers) {
+            if (index % specialNumber == 0) return true;
+        }
+        return false;
     }
 }
